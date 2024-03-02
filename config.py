4df8f74a627ad22f98aa507d545d8b6f0d926ad1
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2024-03-02 14:08:36 +0500

    last commit ":)"

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env.read_env()
 TOKEN = env.str("BOT_TOKEN")
 
 CHANNELS = [
-    ('https://t.me/QuvonchbekBobojonov', '@QuvonchbekBobojonov')
+    ('https://t.me/QuvonchbekBobojonov', '@QuvonchbekBobojonov'),
     ('https://t.me/yangibozorli_volontyor', '@yangibozorli_volontyor')
 ]
 
